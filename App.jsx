0aa44d7ec095b90672ec8afe697d7b74a0db04f4
dhaleprashika <124231072+dhaleprashika@users.noreply.github.com>
2023-03-04 16:59:23 +0530

    Add files via upload
    added

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Account from './function based/Account';
+import About from './function based/About';
+import Menu from './function based/Menu';
+import Home from './function based/Home';
+import Help from './function based/Help';
+import Location from './function based/Location';
+import Logo from './function based/Logo';
+import Search from './function based/Search';
+import Footer from './function based/Footer';
+import Content from './function based/Content';
+
+const App = () => {
+  return (
+    <>
+    <div>App</div>
+    <Account/>
+    <About/>
+    <Menu/>
+    <Home/>
+    <Help/>
+    <Location/>
+    <Logo/>
+    <Search/>
+    <Footer/>
+    <Content/>
+    </>
+  )
+}
+
+export default App
